a4844db3b757527268d75de06d99d751e8adffe8
ip68423 <ilonkapietras@gmail.com>
2018-03-28 11:06:11 +0200

    GuiInput

---
+++ src/pl/waw/sgh/GuiInput.java
@@ -0,0 +1,23 @@
+package pl.waw.sgh;
+
+import javax.swing.*;
+
+public class GuiInput {
+    public static void main (String[] args){
+
+        JOptionPane.showMessageDialog(null, "Hello!");
+      //  String myText = JOptionPane.showInputDialog("What is your name:");
+        // JOptionPane.showMessageDialog(null, "Hello!" +myText + "!!!");
+        String sHeight = JOptionPane.showInputDialog("What is your height in cm");
+        Double height = Double.parseDouble(sHeight);
+        //Integer.parseInt(sHeight);
+        //Long.parseLong(sHeight);
+        JOptionPane.showMessageDialog(null, "Your height in cm: " + height/100);
+
+int res = -1;
+while (res!=0) {
+    res = JOptionPane.showConfirmDialog(null, "Are you sure?");
+    JOptionPane.showMessageDialog(null, "Result: " + res);
+                }
+        }
+    }
