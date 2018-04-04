f0e86bde547a8fcd5ec7441f2efd81a75067c05d
ip68423 <ilonkapietras@gmail.com>
2018-04-04 13:09:30 +0200

    CSV  Text

---
+++ src/pl/waw/sgh/FileReadCSV.java
@@ -0,0 +1,27 @@
+package pl.waw.sgh;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileReadCSV {
+    //  final static String FILE = "h:\\myfile.txt";
+   // final static String FILE = "..\\..\\..\\myfile.txt";
+   final static String FILE = "Book1.csv"; //only for windows
+
+    public static void main(String[] args) {
+        //System.out.println("File separator: " + File.separator);
+        File myFile = new File(FILE);
+        try {
+            Scanner scanner = new Scanner(myFile);
+            int i=0;
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                i++;
+                System.out.println("Line " +i+ " : " + line);
+            }
+        } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
