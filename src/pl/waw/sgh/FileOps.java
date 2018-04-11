d8c1a7572bb164fa537da4451987199bf6a1380e
ip68423 <ilonkapietras@gmail.com>
2018-04-11 11:20:20 +0200

    properties

--- src/pl/waw/sgh/FileOps.java
+++ src/pl/waw/sgh/FileOps.java
@@ -1,11 +1,25 @@
 package pl.waw.sgh;
 
+import java.util.Map;
+
 public class FileOps {
 
     public static void main(String[] args) {
 
         String curDir = System.getProperty("user.dir");
+        String tmpDir = System.getProperty("java.io.tmpdir");
+        System.out.println("TMP: " + tmpDir);
 
+
+        String mySysProp = System.getProperty("prubac.myprop");
+        System.out.println("myprop: " + mySysProp);
+
+        String user = System.getenv("USERNAME");
+        Map<String, String> env = System.getenv();
+        System.out.println(user);
+
+        System.out.println(env.get("HOMEPATH"));
         System.out.println(curDir);
     }
 }
+// java system property    java.io.tempdir
